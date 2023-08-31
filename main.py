ee840630b4a414e47e9209f3cd47da26e62f34c0
Priyanshu Jani <118464046+Vezingg@users.noreply.github.com>
2023-08-31 13:04:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from image_processing import preprocess_image
 from text_extraction import extract_text
 from text_parsing import parse_aadhaar_info
 def main():
-    image=preprocess_image('Pratham.jpg')
+    image=preprocess_image('pratham.jpg')
     extracted_text=extract_text(image)
     aadhaar_info=parse_aadhaar_info(extracted_text)
     if aadhaar_info:
         print("Aadhaar Information:")
         print("Aadhaar Number:",aadhaar_info['aadhaar_number'])
+        print("Birthdate:",aadhaar_info["birthdate"])
     else:
-        print("Aadhaar number not found in the extracted text.")
+        print("Aadhaar information not found in the extracted text.")
 if __name__ == '__main__':
     main()
