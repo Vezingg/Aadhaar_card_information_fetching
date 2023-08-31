d16b84cfaf7f3f5ad0dca85da93fe170928bb708
Priyanshu Jani <118464046+Vezingg@users.noreply.github.com>
2023-08-31 13:04:00 +0530

    Update text_parsing.py

--- text_parsing.py
+++ text_parsing.py
@@ -1,11 +1,18 @@
 import re
+
 def parse_aadhaar_info(extracted_text):
-    aadhaar_number_match=re.search(r'\d{4} \d{4} \d{4}',extracted_text)
-    if aadhaar_number_match:
-        aadhaar_number=aadhaar_number_match.group()
-        aadhaar_info={
+    aadhaar_number_match = re.search(r'\d{4} \d{4} \d{4}', extracted_text)
+    birthdate_match = re.search(r'(?<=DOB : )\d{2}/\d{2}/\d{4}', extracted_text)
+    
+    if aadhaar_number_match and birthdate_match:
+        aadhaar_number = aadhaar_number_match.group()
+        birthdate = birthdate_match.group()
+        
+        aadhaar_info = {
             'aadhaar_number': aadhaar_number,
+            'birthdate': birthdate,
         }
+        
         return aadhaar_info
     else:
-        return None 
+        return None
